761b680f5a34e49ec15be1f580b6155985eda8e6
Prabesh Narsingh Kunwar <prabeshkunwar12@gmail.com>
2024-08-06 16:39:27 -0300

    added cancel buttons

--- gameroomkiosk/pages/registration/index.js
+++ gameroomkiosk/pages/registration/index.js
@@ -38,15 +38,15 @@ const Players = () => {
     }
   };
 
-  const fetchPrimaryPlayer = async () => {
+  const fetchPrimaryPlayer = () => {
     setError('');
-    if (playersList.length > 0) {
-      const id = playersList[0].PlayerID;
-      console.log(id);
-      try{
-        setPrimaryPlayer(await fetchPlayerbyId(id));
-      } catch(err) {
-        setError('Problem finding Primary Player!');
+    if (players.length > 0) {
+      const primaryPlayer = players.find(player => player.PlayerID === player.SigneeID);
+      if (primaryPlayer) {
+        console.log(primaryPlayer.PlayerID, primaryPlayer.SigneeID, primaryPlayer.PlayerID === primaryPlayer.SigneeID);
+        return primaryPlayer;
+      } else {
+        console.error("Primary player not found");
       }
     } else {
       setIsEmailFound(false);
@@ -115,12 +115,14 @@ const Players = () => {
     setError('');
   
     try {
-      const player = await createPrimaryPlayer();
+      const player = players.length > 0 ? fetchPrimaryPlayer(): await createPrimaryPlayer();
+      console.log(player);
       if (player && newKidsForms.length > 0) {
         await createKids(player);
       }
       fetchPlayerByEmail(email); // Refresh players list
     } catch (err) {
+      console.error('Failed to create Players', err);
       setError('Failed to create Players', err);
     } finally {
       setLoading(false);
@@ -153,8 +155,14 @@ const Players = () => {
     setNewKidsForms(updatedForms);
   };
 
-  const handleNewInfoSubmit = (e) => {
+  const handleNewInfoSubmit = async (e) => {
     e.preventDefault();
+    if(signingFor==="existingWaiverAddKids") {
+      await createPlayers();
+      setNewKidsForms([]);
+      setStep(2);
+      return;
+    }
     setStep(4);
   };
 
@@ -211,7 +219,7 @@ const Players = () => {
   return (
     <div className={styles.pageBackground}>
       {step === 1 && (
-        <div className={styles.formContainer}>
+        <div className={styles.container}>
           <h1>Enter Your Email</h1>
           {error && <p style={{ color: 'red' }}>{error}</p>}
           <form onSubmit={handleEmailSubmit}>
@@ -230,7 +238,7 @@ const Players = () => {
       )}
 
       {step === 2 && isEmailFound && (
-        <div className={styles.optionContainer}>
+        <div className={styles.container}>
           <h2>We Found following waivers for this email, please choose one who want to play or add one</h2>
           <ul>
             {players.map(player => (
@@ -241,19 +249,21 @@ const Players = () => {
             ))}
           </ul>
           <button onClick={() => handleSigningOption('existingWaiverAddKids')} className={styles.button}>Add New Waiver</button>
+          <a href=""> <button className={styles.button} disabled={loading}>Cancel</button></a>
         </div>
       )}
 
       {step === 2 && !isEmailFound && (
-        <div className={styles.optionContainer}>
+        <div className={styles.container}>
           <h2>Who would you like to sign a waiver for?</h2>
           <button onClick={() => handleSigningOption('self')} className={styles.button}>Myself</button>
           <button onClick={() => handleSigningOption('selfAndKids')} className={styles.button}>Myself and Kids</button>
+          <a href=""> <button className={styles.button} disabled={loading}>Cancel</button></a>
         </div>
       )}
 
       {step === 3 && signingFor === 'self' && (
-        <div className={styles.formContainer}>
+        <div className={styles.container}>
           <h1>Enter Your Information</h1>
           <form onSubmit={handleNewInfoSubmit}>
             <div className={styles.formRow}>
@@ -297,12 +307,13 @@ const Players = () => {
               />
             </div>
             <button type="submit" className={styles.button} disabled={loading}>Next</button>
+            <a href=""><button className={styles.button} disabled={loading}>Cancel</button></a>
           </form>
         </div>
       )}
 
       {step === 3 && signingFor === 'selfAndKids' && (
-        <div className={styles.formContainer}>
+        <div className={styles.container}>
           <h1>Enter Your Information</h1>
           <form onSubmit={handleNewInfoSubmit}>
             <div className={styles.formRow}>
@@ -383,12 +394,60 @@ const Players = () => {
             ))}
             <button type="button" onClick={handleAddKid} className={styles.button}>Add Child</button>
             <button type="submit" className={styles.button} disabled={loading}>Next</button>
+            <a href=""><button className={styles.button} disabled={loading}>Cancel</button></a>
+          </form>
+        </div>
+      )}
+
+      {step === 3 && signingFor === 'existingWaiverAddKids' && (
+        <div className={styles.container}>
+          <h1>Enter Your Information</h1>
+          <form onSubmit={handleNewInfoSubmit}>
+            <h2>Children's Information</h2>
+            {newKidsForms.map((kid, index) => (
+              <div key={index} className={styles.newKidForm}>
+                <h3 className={styles.kidFormHeading}>Child {index + 1} Information</h3>
+                <div className={styles.formRow}>
+                  <label className={styles.formRowLabel}>First Name:</label>
+                  <input
+                    type="text"
+                    value={kid.FirstName}
+                    onChange={(e) => handleKidChange(index, 'FirstName', e.target.value)}
+                    className={styles.formRowInput}
+                    required
+                  />
+                </div>
+                <div className={styles.formRow}>
+                  <label className={styles.formRowLabel}>Last Name:</label>
+                  <input
+                    type="text"
+                    value={kid.LastName}
+                    onChange={(e) => handleKidChange(index, 'LastName', e.target.value)}
+                    className={styles.formRowInput}
+                    required
+                  />
+                </div>
+                <div className={styles.formRow}>
+                  <label className={styles.formRowLabel}>Date of Birth:</label>
+                  <input
+                    type="date"
+                    value={kid.DateOfBirth}
+                    onChange={(e) => handleKidChange(index, 'DateOfBirth', e.target.value)}
+                    className={styles.formRowInput}
+                    required
+                  />
+                </div>
+              </div>
+            ))}
+            <button type="button" onClick={handleAddKid} className={styles.button}>Add Child</button>
+            <button type="submit" className={styles.button} disabled={loading}>Next</button>
+            <a href=""><button className={styles.button} disabled={loading}>Cancel</button></a>
           </form>
         </div>
       )}
 
       {step === 4 && (
-        <div className={styles.waiverContainer}>
+        <div className={styles.container}>
           <h1>Waiver Agreement</h1>
           <p className={styles.waiverText}>Please read and accept the waiver agreement to proceed:</p>
           <textarea
@@ -407,18 +466,24 @@ const Players = () => {
           />
           <div className={styles.signatureButtons}>
             <button onClick={clearSignature} className={styles.button} disabled={loading}>Clear Signature</button>
-            <button onClick={handleWaiverAccept} className={styles.button} disabled={loading}>Submit</button>
+            <button onClick={handleWaiverAccept} className={styles.button} disabled={loading}>Accept</button>
+            <a href=""><button className={styles.button} disabled={loading}>Decline</button></a>
           </div>
         </div>
       )}
 
       {step === 5 && (
-        <div className={styles.nfcContainer}>
-          <h1>Scan Your Wristband</h1>
-          <p>To complete your registration, please scan your wristband with NFC.</p>
-          <button onClick={handleNFCScan} className={styles.button} disabled={loading}>
-            Start NFC Scan
-          </button>
+        <div className={styles.container}>
+          <h1>Wristband Scanner</h1>
+          {!nfcScanResult && (
+              <>
+                <p>Please scan your wristband.</p>
+                <button onClick={handleNFCScan} className={styles.button} disabled={loading}>
+                  Start Scanning
+                </button>
+              </>
+            )
+          }
           {nfcScanResult && (
             <div className={styles.nfcResult}>
               <p>NFC Scan Result: {nfcScanResult}</p>
@@ -433,12 +498,12 @@ const Players = () => {
 
 
       {step === 6 && (
-        <div className={styles.thankYouContainer}>
+        <div className={styles.container}>
           <h1>Thank You!</h1>
           {selectedWaiver ? (
-            <p>You have selected an existing waiver. Have a great time!</p>
+            <p>Thank you for returning. Have a great time!</p>
           ) : (
-            <p>Your registration and waiver have been successfully submitted. Have a great time!</p>
+            <p>Your registration and waiver has been successfully submitted. Have a great time!</p>
           )}
           <a href=""> <button className={styles.button} disabled={loading}>Start Over</button></a>
         </div>
