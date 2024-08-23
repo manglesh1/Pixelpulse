ff1911a868ffabfd4cccc50ab4a50385afbb0e3a
manglesh1 <nmanglesh@suncor.com>
2024-08-23 15:45:11 -0400

    fied

--- rest/models/gamesVariant.js
+++ rest/models/gamesVariant.js
@@ -35,25 +35,21 @@ module.exports = (sequelize, DataTypes) => {
       },
       MaxIterations: {
         type: DataTypes.INTEGER,
-        allowNull: false,
-        defaultValue: 5
+        allowNull: false//,        defaultValue: 5
       },
       MaxIterationTime: {
         type: DataTypes.INTEGER,
-        allowNull: false,
-        defaultValue: 30
+        allowNull: false//,        defaultValue: 30
       },
       MaxLevel: {
         type: DataTypes.INTEGER,
-        allowNull: false,
-        defaultValue: 10
+        allowNull: false//,        defaultValue: 10
       },
       ReductionTimeEachLevel: {
         type: DataTypes.INTEGER,
-        allowNull: false,
-        defaultValue: 5
+        allowNull: false//,        defaultValue: 5
       },
-      
+
       GameId: {
         type: DataTypes.INTEGER,
         references: {
