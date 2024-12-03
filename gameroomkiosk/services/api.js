e4e04699c21dc56cc3f0f597c78bcb015cb3a014
Prabesh Narsingh Kunwar <prabeshkunwar12@gmail.com>
2024-12-03 12:11:54 -0500

    .env added in gameroomkiosk

--- gameroomkiosk/services/api.js
+++ gameroomkiosk/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8080/api';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export const fetchPlayerbyId = async (id) => {
     const res = await axios.get(`${API_BASE_URL}/player/${id}`)
