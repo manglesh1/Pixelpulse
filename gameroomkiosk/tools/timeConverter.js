6ca41e4c21f5f38b811ecfbde12e12cc3fedb739
Andrew Dionne <126816983+rxsq@users.noreply.github.com>
2025-07-16 20:29:42 -0400

    Fix: GameSelection | Time Conversion Issues + Reset Button Hover

--- gameroomkiosk/tools/timeConverter.js
+++ gameroomkiosk/tools/timeConverter.js
@@ -7,18 +7,18 @@ function time(minutes) {
     let remainingMinutes = minutes;
 
     const years = Math.floor(remainingMinutes / (MINUTES_IN_HOUR * HOURS_IN_DAY * DAYS_IN_MONTH * MONTHS_IN_YEAR));
-    remainingMinutes %= MINUTES_IN_HOUR * HOURS_IN_DAY * DAYS_IN_MONTH * MONTHS_IN_YEAR;
+    remainingMinutes -= years * MINUTES_IN_HOUR * HOURS_IN_DAY * DAYS_IN_MONTH * MONTHS_IN_YEAR;
 
     const months = Math.floor(remainingMinutes / (MINUTES_IN_HOUR * HOURS_IN_DAY * DAYS_IN_MONTH));
-    remainingMinutes %= MINUTES_IN_HOUR * HOURS_IN_DAY * DAYS_IN_MONTH;
+    remainingMinutes -= months * MINUTES_IN_HOUR * HOURS_IN_DAY * DAYS_IN_MONTH;
 
     const days = Math.floor(remainingMinutes / (MINUTES_IN_HOUR * HOURS_IN_DAY));
-    remainingMinutes %= MINUTES_IN_HOUR * HOURS_IN_DAY;
+    remainingMinutes -= days * MINUTES_IN_HOUR * HOURS_IN_DAY;
 
     const hours = Math.floor(remainingMinutes / MINUTES_IN_HOUR);
-    remainingMinutes %= MINUTES_IN_HOUR;
+    remainingMinutes -= hours * MINUTES_IN_HOUR;
 
-    return { years, months, days, hours, minutes };
+    return { years, months, days, hours, minutes: remainingMinutes };
 }
 
 module.exports = time;
