69d26e8abc4b778b60bc3dc196a3ee3585f57fda
Andrew Dionne <126816983+rxsq@users.noreply.github.com>
2026-03-22 15:00:42 -0400

    fix: increase payload size to 10mb

--- rest/app.js
+++ rest/app.js
@@ -47,8 +47,8 @@ app.use(cors(corsOptions));
 
 // parser
 app.use(cookieParser());
-app.use(express.json({ limit: "2mb" }));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 // context, limits, and logging
 app.use(requestContext(logger));
