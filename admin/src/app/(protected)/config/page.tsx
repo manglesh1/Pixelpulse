92b79f8426d8f0328615c67ff1b9f4203942f2f6
Andrew Dionne <126816983+rxsq@users.noreply.github.com>
2026-03-26 19:05:16 -0400

    fix: next.js build error

--- admin/src/app/(protected)/config/page.tsx
+++ admin/src/app/(protected)/config/page.tsx
@@ -1,5 +1,5 @@
 import ConfigsTable from "@/features/configs/components/ConfigsTable";
-import { getCurrentUser } from "@/features/admin/server/client";
+import { getCurrentUser } from "@/features/admin/server/user";
 
 export default async function ConfigPage() {
   const user = await getCurrentUser();
